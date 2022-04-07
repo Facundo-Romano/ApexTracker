a7ffdd2d63456f12d973a949601804fe076543ae
Facundo-Romano <facuromano121299@gmail.com>
2022-04-07 10:06:29 -0300

    new env variables

--- src/db.js
+++ src/db.js
@@ -5,14 +5,16 @@ const path = require("path");
 const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME, HEROKU_DB_USER, 
 	    HEROKU_DB_PASSWORD, HEROKU_DB_HOST, HEROKU_DB_NAME, NODE_ENV } = process.env;
 
+let sequelize = null;
+
 if (NODE_ENV !== "production") {
-	const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`,
+	sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`,
 	{
 		logging: false, // set to console.log to see the raw SQL queries
 		native: false, // lets Sequelize know we can use pg-native for ~30% more speed
 	})
 } else {
-	const sequelize = new Sequelize(`postgres://${HEROKU_DB_USER}:${HEROKU_DB_PASSWORD}@${HEROKU_DB_HOST}/${HEROKU_DB_NAME}`,
+	sequelize = new Sequelize(`postgres://${HEROKU_DB_USER}:${HEROKU_DB_PASSWORD}@${HEROKU_DB_HOST}/${HEROKU_DB_NAME}`,
 	{
 		logging: false, // set to console.log to see the raw SQL queries
 		native: false, // lets Sequelize know we can use pg-native for ~30% more speed
