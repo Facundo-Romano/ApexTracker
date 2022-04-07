6836c7b339d5f6d495f6f89446166598ebc52057
Facundo-Romano <facuromano121299@gmail.com>
2022-04-07 10:17:48 -0300

    port fix

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const server = require("./src/app.js");
 const { conn } = require("./src/db.js");
 const { LOAD_MOCK_DATA } = process.env;
 // Syncing all the models at once.
+
+const PORT = process.env.PORT || 3001;
+
 conn
   .sync({ force: true })
   .then(() => {
@@ -12,8 +15,8 @@ conn
     conn.query("CREATE EXTENSION IF NOT EXISTS  pg_trgm;");
   })
   .then(() => {
-    server.listen(3001, () => {
-      console.log("%s listening at 3001"); // eslint-disable-line no-console
+    server.listen(PORT, () => {
+      console.log(`%s listening at ${PORT}`); // eslint-disable-line no-console
     });
   })
   .catch((err) => {
